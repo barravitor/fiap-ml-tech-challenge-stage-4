32387fb78890ba98d2fa794b83110ebf3972f50c
Edson Vitor <edson.vitor42@gmail.com>
2025-06-06 12:04:18 -0300

    chore: refactor build to use railway

--- mlflow_server/start_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "[MLFLOW] Starting MLflow server..."
-
-mlflow server \
-  --backend-store-uri ./mlruns \
-  --default-artifact-root ./mlruns \
-  --host 0.0.0.0 \
-  --port 5000
