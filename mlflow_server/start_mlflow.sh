f7e9ef58d6bbd190583d02df0914c882547f1eca
Edson Vitor <edson.vitor42@gmail.com>
2025-06-07 15:46:31 -0300

    chore: test

--- mlflow_server/start_mlflow.sh
+++ mlflow_server/start_mlflow.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Aguarda até que o NGINX esteja escutando na porta 80
+echo "Aguardando o NGINX ficar disponível..."
+while ! nc -z localhost 80; do
+  sleep 1
+done
+
+echo "NGINX disponível. Iniciando MLflow..."
+
 DB_PATH=$(pwd)/data/mlflow.db
 ARTIFACT_PATH=$(pwd)/data/mlartifacts
 
