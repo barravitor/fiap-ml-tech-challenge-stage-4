5587ecc6c222a6dcc36d04bc90726c875cebcf9d
Edson Vitor <edson.vitor42@gmail.com>
2025-06-07 20:56:07 -0300

    feat: config cors rules

--- api/app/main.py
+++ api/app/main.py
@@ -1,6 +1,7 @@
 # app/main.py
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routes.index_routes import router
 
 @asynccontextmanager
@@ -31,6 +32,19 @@ app = FastAPI(
         "name": "Predict",
         "description": "Operations related to predict stocks price."
     }]
+)
+
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(router)
