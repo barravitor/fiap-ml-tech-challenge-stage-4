88057d06459499b24b9daef7c1645b87113c8f75
Edson Vitor <edson.vitor42@gmail.com>
2025-06-07 16:24:05 -0300

    chore: test

--- mlflow_server/start_mlflow.sh
+++ mlflow_server/start_mlflow.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+echo "⏳ Aguardando MinIO iniciar..."
+until curl -s http://localhost:9000/minio/health/ready | grep -q "OK"; do
+  sleep 1
+done
+
+echo "✅ MinIO está pronto. Iniciando MLflow..."
+
 DB_PATH=$(pwd)/data/mlflow.db
 ARTIFACT_PATH=$(pwd)/data/mlartifacts
 
+# Cria banco se não existir
+touch $DB_PATH
+
 mlflow server \
   --backend-store-uri sqlite:///$DB_PATH \
-  --default-artifact-root file:///$ARTIFACT_PATH \
+  --default-artifact-root s3://mlflow-artifacts \
   --host 0.0.0.0 \
   --port 5000
